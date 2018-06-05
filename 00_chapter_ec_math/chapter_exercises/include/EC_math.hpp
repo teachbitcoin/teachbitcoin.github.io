73d37ca792df99f2e8b001afdf1f8bf9dfe9a028
James Chiang <james.chiangwu@gmail.com>
2018-06-05 20:41:08 +0200

    rm ops also commited

--- 00_chapter_ec_math/chapter_exercises/include/EC_math.hpp
+++
@@ -1,8 +0,0 @@
-#include <bitcoin/bitcoin.hpp>
-#include <iostream>
-
-void ec_math_exercise_1a();
-void ec_math_exercise_1b();
-void ec_math_exercise_1c();
-
-bc::byte_array<bc::ec_secret_size> int_to_bytes(bc::uint256_t my_int);
